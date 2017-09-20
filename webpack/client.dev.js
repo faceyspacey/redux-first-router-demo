4cd40ee2af96da88ced5b20a109079518248e61a
re6exp <re6exp@gmail.com>
2017-09-20 18:11:34 +0300

    Fixes for SEO with basename

--- webpack/client.dev.js
+++ webpack/client.dev.js
@@ -12,7 +12,7 @@ module.exports = {
   entry: [
     'babel-polyfill',
     'fetch-everywhere',
-    'webpack-hot-middleware/client?path=/__webpack_hmr&timeout=20000&reload=false&quiet=false&noInfo=false',
+    'webpack-hot-middleware/client?path=http://localhost:3000/__webpack_hmr&timeout=20000&reload=true&quiet=false&noInfo=false',
     'react-hot-loader/patch',
     path.resolve(__dirname, '../src/index.js')
   ],
