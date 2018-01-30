bd258aded0ed655ca9a9f4923257bb31c4fc91ac
Martiuh <martiuh@gmail.com>
2018-01-30 16:53:48 -0600

    add plugins to the plugin AutoDLL in webpack/client.prod.js to properly uglify vendor

--- webpack/client.prod.js
+++ webpack/client.prod.js
@@ -89,7 +89,29 @@ module.exports = {
           'babel-polyfill',
           'redux-devtools-extension/logOnlyInProduction'
         ]
-      }
+      },
+      plugins: [
+        new webpack.DefinePlugin({
+          'process.env': {
+            NODE_ENV: JSON.stringify('production')
+          }
+        }),
+
+        new webpack.optimize.UglifyJsPlugin({
+          compress: {
+            screw_ie8: true,
+            warnings: false
+          },
+          mangle: {
+            screw_ie8: true
+          },
+          output: {
+            screw_ie8: true,
+            comments: false
+          },
+          sourceMap: true
+        })
+      ]
     })
   ]
 }
